df64084b8d05225fba671568d055a4c32dd240ed
Reeha Parkar <reeha2411@gmail.com>
2025-06-28 14:03:37 +0100

    added data analysis and organized folder

---
+++ CMU-MultimodalSDK-Tutorials/constants/paths.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+# path to the SDK folder
+SDK_PATH: Optional[str] = "C:/Users/Reeha Parkar/OneDrive/Desktop/Workspace/KCL/Semester 2/Project/mer-project/CMU-MultimodalSDK"
+
+# path to the folder where you want to store data
+DATA_PATH: Optional[str] = './data/'
+
+# path to a pretrained word embedding file
+WORD_EMB_PATH: Optional[str] = None
+
+# path to loaded word embedding matrix and corresponding word2id mapping
+CACHE_PATH: Optional[str] = './data/embedding_and_mapping.pt'
+
